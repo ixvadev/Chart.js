8388fc35b5695f67c19430c0015a216277a10a11
Nodirbek <khujamovcodes@gmail.com>
2024-07-05 17:13:49 +0500

    chart.js

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
-const ctx = document.getElementById('myChart');
+let elList = document.getElementById("demo");
 
-new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
-      }
-    }
-  }
-});
+let API = "https://dummyjson.com/products";
+
+fetch(API).then((response) => response.json()).then((data) => {
+        data.products.map((item) => {
+        const ctx = document.getElementById('myChart');
+      
+        new Chart(ctx, {
+          type: 'doughnut',
+          data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange', '', 'Blue'],
+            datasets: [{
+              label: '# of Votes',
+              data: [item.rating, item.rating, item.price, item.discountPercentage, item.price, item.rating, item.rating, item.price, item.rating, item.price, item.price, item.stock, item.price, item.discountPercentage],
+              borderWidth: 2,
+              width:"10px",
+              color: "red"
+            }]
+          },
+          options: {
+            scales: {
+              y: {
+                beginAtZero: true
+              }
+            }
+          }
+        });
+    });
+})
